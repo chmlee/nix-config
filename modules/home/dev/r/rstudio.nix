a70c75c5630e060c9b673e764be5bee3b340afd3
Louis Chih-Ming Lee <louis@louisclee.com>
2026-06-05 13:32:36 +0200

    fix rstudio/electron rendering error under wayland

--- modules/home/dev/r/rstudio.nix
+++ modules/home/dev/r/rstudio.nix
@@ -1,4 +1,20 @@
 { pkgs, rPkgs }:
-pkgs.rstudioWrapper.override {
-  packages = rPkgs;
+
+let
+  rstudioWithPackages = pkgs.rstudioWrapper.override {
+    packages = rPkgs;
+  };
+in
+pkgs.symlinkJoin {
+  name = "rstudio-wayland";
+  paths = [ rstudioWithPackages ];
+  buildInputs = [ pkgs.makeWrapper ];
+
+  postBuild = ''
+    mv $out/bin/rstudio $out/bin/rstudio-wayland
+
+    wrapProgram $out/bin/rstudio-wayland \
+      --add-flags "--ozone-platform=wayland" \
+      --add-flags "--enable-features=WaylandWindowDecorations"
+  '';
 }
